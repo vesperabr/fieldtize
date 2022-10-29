b0e9212e9f759e82b1a4dc8ab68261f1cfa9e186
Ricardo Monteiro <ricardo@lab12.com.br>
2022-10-29 15:01:42 -0300

    chore: Implements Field class attributes

--- src/Fields/Field.php
+++ src/Fields/Field.php
@@ -4,4 +4,33 @@ namespace Vespera\Fieldtize\Fields;
 
 abstract class Field
 {
+    protected string $original;
+    protected ?string $value = null;
+    protected ?string $raw = null;
+    protected bool $isValid = false;
+
+    public function original(): string
+    {
+        return $this->original;
+    }
+
+    public function get(): ?string
+    {
+        return $this->value;
+    }
+
+    public function raw(): ?string
+    {
+        return $this->raw;
+    }
+
+    public function isValid(): bool
+    {
+        return $this->isValid;
+    }
+
+    public function __toString()
+    {
+        return $this->get() ?? '';
+    }
 }
